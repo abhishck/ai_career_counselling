c58a6a13458ffbf61df1853831cc16874bc5c0f5
Abhishek Rathore <abhishekrathore94500@gmail.com>
2026-03-16 01:50:59 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Home from './pages/Home'
-import Navbar from './components/Navbar.jsx'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <>
-    <Navbar/>
-   <Home/>
-   </>
-  )
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
