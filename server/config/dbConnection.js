c053c435783a839eb949e9d73d4cf2afa678acbd
Abhishek Rathore <abhishekrathore94500@gmail.com>
2026-03-18 22:15:53 +0530

    resolve database connection

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     console.log("CONNECTION_STRING:", process.env.CONNECTION_STRING);
+    const CONNECTION_STRING="mongodb+srv://abhishekrathore94500_db_user:admin@cluster0.l3cf7de.mongodb.net/UserAuth?appName=Cluster0"
 
-    const dB = await mongoose.connect(process.env.CONNECTION_STRING);
+    const dB = await mongoose.connect(CONNECTION_STRING);
 
     console.log(
       `✅ database connected : ${dB.connection.name} ${dB.connection.host}`
